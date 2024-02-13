460e2fc652d74e71229616d5e19664e232f5d79b
Виталий Пахомович <warfire122@gmail.com>
2024-02-13 16:36:06 +0800

    Навалил кринжа со старых библиотек, чтобы создать бд

---
+++ DefaultCRUDGRX.BL/Interfaces/IMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefaultCRUDGRX.BL.Interfaces
+{
+    public interface IMap<T,TDTO>
+    {
+        TDTO Down(T model);
+        T Up(TDTO modelDTO);
+    }
+}
---
+++ DefaultCRUDGRX.BL/Mappers/ToDoMapper.cs
@@ -0,0 +1,20 @@
+﻿using DefaultCRUDGRX.BL.DTO;
+using DefaultCRUDGRX.BL.Interfaces;
+using DefaultCRUDGRX.DAL.Models;
+
+namespace DefaultCRUDGRX.BL.Mappers
+{
+    public static class ToDoMapper : IMap<ToDo, ToDoDTO>
+    {
+        
+        public static ToDoDTO Down(ToDo model)
+        {
+            return new ToDoDTO() { Name = model.Name, Description = model.Description, Status_Name = model!.Status!.Status_Name};
+        }
+
+        public static ToDo Up(ToDoDTO modelDTO)
+        {
+            return new ToDo() { Name = modelDTO.Name, Description = modelDTO.Description };
+        }
+    }
+}
